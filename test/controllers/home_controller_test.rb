f32f533d4cd176934e26be49571dfb16b7c854cf
Richard Jackson <jackson.richard420@gmail.com>
2018-05-14 16:57:49 +0200

    test views

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,32 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get home_index_url
+  
+  test "should get root" do
+    get home_index_path
     assert_response :success
   end
+
+  test "should get all users" do
+    get home_show_path
+    assert_response :success
+  end
+
+  test "should get registration page" do
+    get new_user_registration_path
+    assert_response :success
+  end
+
+   test "should get edit page" do
+    get edit_user_registration_path
+    assert_response :success
+  end
+
+  test "should get session login page" do
+    get new_user_session_path
+    assert_response :success
+  end
+
+
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  
   # test "the truth" do
   #   assert true
   # end
+ 
+  def setup
+    @user = User.new(first_name: 'John François', last_name: 'Begood', email: 'john@example.com', password:'huzefznpzef')
+  	@user.valid?
+  end
+
+  test 'valid user' do
+    assert @user.valid?
+  end
+
+  test 'invalid without first name' do
+    @user.first_name = nil
+    refute @user.valid?, 'saved user without a name'
+    #assert_not_nil @user.errors[:first_name], 'no validation error for name present'
+  end
+
+  test 'invalid without blanks' do
+    @user.first_name = '       '
+    refute @user.valid?, 'saved user without a name'
+    #assert_not_nil @user.errors[:first_name], 'no validation error for name present'
+  end
+
+  test 'invalid without last name' do
+    @user.last_name = nil
+    refute @user.valid?, 'saved user without a name'
+    #assert_not_nil @user.errors[:last_name], 'no validation error for name present'
+  end
+
+  test 'invalid without email' do
+    @user.email = nil
+    refute @user.valid?
+    #assert_not_nil @user.errors[:email]
+  end
+
+  test 'email already taken' do
+    @user.email = User.exists?(:email => @user.email)
+    refute @user.valid?
+    #assert_not_nil @user.errors[:email]
+  end
+
+  test 'invalid without password' do
+    @user.password = nil
+    refute @user.valid?
+    #assert_not_nil @user.errors[:email]
+  end
+
 end
